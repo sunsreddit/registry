23b596ed12c353bcfd1fa9440871e4f8cdd4638b
bruxc <bruxc@sunsreddit.net>
2024-01-30 21:23:52 -0500

    Adds new repositories to org: `sunsreddit.net`
    - sunsreddit/gamethread
    - sunsreddit/nba-stats
    - sunsreddit/registry
    - sunsreddit/sidebar

--- lib/registry-stack.ts
+++ lib/registry-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as parameters from '../config/parameters.json';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
 
 export class RegistryStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,8 +10,12 @@ export class RegistryStack extends cdk.Stack {
     
     const org = resources.github.organization;
     const repos = resources.github.repos;
-    repos.forEach((repo) => {
-      console.log(`${org}/${repo.name}`);
+    repos.forEach((repo: { name: string; }) => {
+      const repoName = repo.name;
+      new Repository(this, `${repoName}Repo`, {
+        repositoryName: `${org}/${repoName}`,
+        imageScanOnPush: true
+      });
     });
 
   }
